2fc41c148627e8c1de405b0fcd1e09be4a533866
Martin Kyjac <martin.kyjac@bluesoft.cz>
2024-04-11 08:35:35 +0200

    Create blank admin UI application with UIPage for editing shopify configuration

---
+++ src/Kentico.Xperience.Shopify/Admin/ShopifyIntegrationSettingsEdit.cs
@@ -0,0 +1,31 @@
+﻿using Kentico.Xperience.Admin.Base;
+using Kentico.Xperience.Admin.Base.Forms;
+using Kentico.Xperience.Shopify.Admin;
+
+[assembly: UIPage(
+    parentType: typeof(ShopifyIntegrationSettingsApplication),
+    slug: "shopify-settings-edit",
+    uiPageType: typeof(ShopifyIntegrationSettingsEdit),
+    name: "Shopify settings",
+    templateName: TemplateNames.EDIT,
+    order: UIPageOrder.First)]
+
+namespace Kentico.Xperience.Shopify.Admin
+{
+    public class ShopifyIntegrationSettingsEdit : ModelEditPage<ShopifySettingsModel>
+    {
+        public ShopifyIntegrationSettingsEdit(Xperience.Admin.Base.Forms.Internal.IFormItemCollectionProvider formItemCollectionProvider, IFormDataBinder formDataBinder) : base(formItemCollectionProvider, formDataBinder)
+        {
+        }
+
+        private ShopifySettingsModel? model;
+
+        protected override ShopifySettingsModel Model => model ??= new ShopifySettingsModel() { ShopifyStoreUrl = "test" };
+
+        protected override Task<ICommandResponse> ProcessFormData(ShopifySettingsModel model,
+       ICollection<IFormItem> formItems)
+        {
+            return base.ProcessFormData(model, formItems);
+        }
+    }
+}
---
+++ src/Kentico.Xperience.Shopify/Admin/ShopifySettingsModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Configuration;
+using Kentico.Xperience.Admin.Base.FormAnnotations;
+
+namespace Kentico.Xperience.Shopify.Admin
+{
+    public class ShopifySettingsModel
+    {
+        [UrlValidationRule]
+        [TextInputComponent(Label = "Shopify store URL", Order = 1)]
+        [RequiredValidationRule]
+        public string? ShopifyStoreUrl { get; set; }
+
+
+        [TextInputComponent(Label = "Admin API key", Order = 2)]
+        [RequiredValidationRule]
+        public string? AdminApiKey { get; set; }
+
+
+        [TextInputComponent(Label = "Storefront API key", Order = 3)]
+        [RequiredValidationRule]
+        public string? StorefrontApiKey { get; set; }
+
+
+        [TextInputComponent(Label = "Storefront API version", Order = 4)]
+        [Description("Api version in format YYYY-MM. Admin API version is not needed since it is set by ShopifySharp NuGet package version")]
+        [RegexStringValidator(@"[0-9]{4}-[0-9]{2}")]
+        [RequiredValidationRule]
+        public string? StorefrontApiVersion { get; set; }
+
+
+        [ObjectIdSelectorComponent(CurrencyFormatInfo.OBJECT_TYPE, Label = "Currency format", Order = 5)]
+        [RequiredValidationRule]
+        public IEnumerable<string>? CurrencyFormats { get; set; }
+    }
+}
