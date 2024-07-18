93cf46d382130cc858b242debe70f1adc68824e9
Jiří Češka <ceska@bluesoft.cz>
2024-07-18 10:14:41 +0200

    Synchronize content items to content item folder.

---
+++ src/Kentico.Xperience.K13Ecommerce/Synchronization/ContentItems/ContentItemFolderSynchronizationService.cs
@@ -0,0 +1,35 @@
+﻿using CMS.ContentEngine;
+
+using K13Store;
+
+namespace Kentico.Xperience.K13Ecommerce.Synchronization.ContentItems;
+
+internal class ContentItemFolderSynchronizationService : IContentItemFolderSynchronizationService
+{
+    private readonly IContentFolderManager contentFolderManager;
+    private readonly IContentQueryExecutor contentQueryExecutor;
+
+    //TODO: From module
+    private const int folderId = 2;
+
+
+    public ContentItemFolderSynchronizationService(IContentFolderManager contentFolderManager, IContentQueryExecutor contentQueryExecutor)
+    {
+        this.contentFolderManager = contentFolderManager;
+        this.contentQueryExecutor = contentQueryExecutor;
+    }
+
+    public async Task SynchronizeContentItemFolders(CancellationToken cancellationToken = default)
+    {
+        await SynchronizeProductFolder(folderId, ProductSKU.CONTENT_TYPE_NAME, cancellationToken);
+        //await SynchronizeProductFolder(folderId, ProductImage.CONTENT_TYPE_NAME, cancellationToken);
+        //await SynchronizeProductFolder(folderId, ProductVariant.CONTENT_TYPE_NAME, cancellationToken);
+    }
+
+    private async Task SynchronizeProductFolder(int folderId, string contentTypeName, CancellationToken cancellationToken = default)
+    {
+        var builder = new ContentItemQueryBuilder().ForContentType(contentTypeName);
+        var ids = await contentQueryExecutor.GetResult(builder, rowData => rowData.ContentItemID, cancellationToken: cancellationToken);
+        await contentFolderManager.MoveItems(folderId, ids, cancellationToken);
+    }
+}
---
+++ src/Kentico.Xperience.K13Ecommerce/Synchronization/ContentItems/IContentItemFolderSynchronizationService.cs
@@ -0,0 +1,13 @@
+﻿namespace Kentico.Xperience.K13Ecommerce.Synchronization.ContentItems;
+
+/// <summary>
+/// Product folder synchronization service.
+/// </summary>
+public interface IContentItemFolderSynchronizationService
+{
+    /// <summary>
+    /// Synchronize XbK content items to selected destination folder.
+    /// </summary>
+    /// <returns></returns>
+    Task SynchronizeContentItemFolders(CancellationToken cancellationToken = default);
+}
