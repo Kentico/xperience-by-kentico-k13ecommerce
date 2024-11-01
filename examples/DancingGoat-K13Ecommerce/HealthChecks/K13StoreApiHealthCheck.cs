658483fcf34c2534d3c267bcfdcf2dbd302359d2
Jiří Češka <ceska@bluesoft.cz>
2024-11-01 14:49:55 +0100

    Add loging to health check.

--- examples/DancingGoat-K13Ecommerce/HealthChecks/K13StoreApiHealthCheck.cs
+++ examples/DancingGoat-K13Ecommerce/HealthChecks/K13StoreApiHealthCheck.cs
@@ -5,8 +5,13 @@ namespace DancingGoat.HealthChecks
     internal class K13StoreApiHealthCheck : IHealthCheck
     {
         private readonly IHttpClientFactory httpClientFactory;
+        private readonly ILogger<K13StoreApiHealthCheck> logger;
 
-        public K13StoreApiHealthCheck(IHttpClientFactory httpClientFactory) => this.httpClientFactory = httpClientFactory;
+        public K13StoreApiHealthCheck(IHttpClientFactory httpClientFactory, ILogger<K13StoreApiHealthCheck> logger)
+        {
+            this.httpClientFactory = httpClientFactory;
+            this.logger = logger;
+        }
 
         public async Task<HealthCheckResult> CheckHealthAsync(
         HealthCheckContext context,
@@ -15,8 +20,7 @@ namespace DancingGoat.HealthChecks
             try
             {
                 using var httpClient = httpClientFactory.CreateClient(nameof(K13StoreApiHealthCheck));
-                var response = await httpClient.GetAsync("/status", cancellationToken);
-
+                var response = await httpClient.PostAsync("/api/store/auth/token", new StringContent("test"));
                 if (response.IsSuccessStatusCode)
                 {
                     return HealthCheckResult.Healthy("App is healthy.");
@@ -35,11 +39,13 @@ namespace DancingGoat.HealthChecks
                     string errorMessage = $"Request to K13 Store API failed with status code {(int)statusCode} ({statusCode}). " +
                         $"Reason: {reasonPhrase}. " +
                         $"Response: {responseBody}";
+                    logger.LogError(errorMessage);
                     return HealthCheckResult.Unhealthy(errorMessage);
                 }
             }
             catch (HttpRequestException e)
             {
+                logger.LogError(e, e.Message);
                 return HealthCheckResult.Unhealthy(exception: e);
             }
         }
